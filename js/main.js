e6a4e8d40c604ff324dd891732bb28570bd4f44e
yunerkis <yumycary@gmail.com>
2017-03-16 01:18:06 -0400

    Structure Modal

--- js/main.js
+++ js/main.js
@@ -1,24 +1,81 @@
-var Hero_List = angular.module("Hero_List", []);
+var Hero_List = angular.module("Hero_List", ['angularUtils.directives.dirPagination']);
 
-Hero_List.controller('List', function($scope, $http) {
-  var all_heros = []
+Hero_List.config(function(paginationTemplateProvider) {
+    paginationTemplateProvider.setPath('dirPagination.tpl.html');
+}).controller('List', function($scope, $http) {
   $scope.heros = []
-
+  $scope.search = function () {
+    if ($scope.filter == ''){
+      var data = { 
+        apikey: "59e7519bf8b3d2546d15e89e60dabc44",
+        nameStartsWith: $scope.filter,
+        limit: 100
+      }
+    }else{
+      var data = { 
+        apikey: "59e7519bf8b3d2546d15e89e60dabc44",
+        nameStartsWith: $scope.filter,
+        limit: 100
+      }
+    }
+    $http({
+      method: 'GET',
+      url: 'http://gateway.marvel.com/v1/public/characters',
+      params: data
+    }).then(function successCallback(response) {
+      $scope.heros = response.data.data.results
+    }, function errorCallback(response) {
+    });
+  }
+  $scope.order_by = function () {
+    if ($scope.filter == ''){
+      var data = { 
+        apikey: "59e7519bf8b3d2546d15e89e60dabc44",
+        nameStartsWith: $scope.filter,
+        limit: 100,
+        orderBy: $scope.order
+      }
+    }else{
+      var data = { 
+        apikey: "59e7519bf8b3d2546d15e89e60dabc44",
+        nameStartsWith: $scope.filter,
+        limit: 100,
+        orderBy: $scope.order
+      }
+    }
+    $http({
+      method: 'GET',
+      url: 'http://gateway.marvel.com/v1/public/characters',
+      params: data
+    }).then(function successCallback(response) {
+      $scope.heros = response.data.data.results
+    }, function errorCallback(response) {
+    });
+  }
+  $scope.select_comic = function (comic) {
+    $http({
+      method: 'GET',
+      url: comic.resourceURI,
+      params:{
+        apikey: "59e7519bf8b3d2546d15e89e60dabc44",
+      }
+    }).then(function successCallback(response) {
+      console.log(response.data.data.results)
+      $scope.comic = response.data.data.results[0]
+    }, function errorCallback(response) {
+    });
+  }
   $http({
     method: 'GET',
     url: 'http://gateway.marvel.com/v1/public/characters',
     params: { 
       apikey: "59e7519bf8b3d2546d15e89e60dabc44",
-      limit: "10",
+      limit: 100
     }
   }).then(function successCallback(response) {
-    console.log(response.data.data.results);
-    all_heros = response.data.data.results
     $scope.heros = response.data.data.results
+    console.log($scope.heros)
   }, function errorCallback(response) {
-    console.log(response.data);
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
   });
 });
 
